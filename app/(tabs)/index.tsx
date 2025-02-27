e24d0e3829dab51f120a52840165efc59f6fe945
Douglas Lowder <douglowder@mac.com>
2025-02-27 12:51:53 -0800

    Remove unneeded components and metro config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Image, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -20,7 +19,6 @@ export default function HomeScreen() {
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">TV demos using react-native-skia</ThemedText>
-        <HelloWave />
       </ThemedView>
     </ParallaxScrollView>
   );

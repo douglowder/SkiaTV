35ac554a125cb43ab63f53f61694546c46798ac2
Douglas Lowder <douglowder@mac.com>
2025-02-27 12:48:28 -0800

    Fix layouts for bottom tabs

--- components/SkiaDemoScreen.tsx
+++ components/SkiaDemoScreen.tsx
@@ -3,7 +3,8 @@ import { StyleSheet, TVFocusGuideView } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useScale } from '@/hooks/useScale';
-import { Breathe } from '@/components/SkiaDemos/Breathe';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export const SkiaDemoScreen = ({
   title,
@@ -14,24 +15,31 @@ export const SkiaDemoScreen = ({
 }) => {
   const styles = useDemoStyles();
   return (
-    <TVFocusGuideView autoFocus style={{ height: '100%' }}>
-      <ThemedView>
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">{title}</ThemedText>
-        </ThemedView>
-        {children}
+    <TVFocusGuideView autoFocus style={styles.container}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">{title}</ThemedText>
       </ThemedView>
+      {children}
     </TVFocusGuideView>
   );
 };
 
 const useDemoStyles = function () {
   const { scale } = useScale();
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? 'light'];
   return StyleSheet.create({
+    container: {
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      backgroundColor: colors.background,
+    },
     titleContainer: {
       flexDirection: 'row',
       gap: 8 * scale,
       margin: 20 * scale,
+      marginTop: 100 * scale,
     },
   });
 };
